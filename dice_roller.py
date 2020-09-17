14a9ada966abd24addef1176d7204627ae746666
JLormine <70603400+JLormine@users.noreply.github.com>
2020-09-17 13:44:20 -0400

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -1,11 +1,11 @@
 def main():
   #print('You rolled a die')
 
-    import random
-    roll = random.randint(1,6)
-    print(f'your roled a{roll}')
+   import random
+   roll = random.randint(1,6)
+   print(f'your roled a{roll}')
  
-    if __name__== "__main__":
+if __name__== "__main__":
 main()
   
   git add dice_roller.py
