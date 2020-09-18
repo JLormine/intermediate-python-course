6f181fd8f31ba3515a3771770c8bca34cbdc96b1
JLormine <70603400+JLormine@users.noreply.github.com>
2020-09-18 09:39:45 -0400

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -1,11 +1,17 @@
 def main():
-  #print('You rolled a die')
-
-   import random
-   roll = random.randint(1,6)
-   print(f'your roled a{roll}')
- 
-if __name__== "__main__":
+    
+dice_rolls = 2
+dice_sum = 0
+for i in range(0,dice_rolls):
+  roll = random.randint(1,6)
+  dice_sum += roll
+  print(f'You rolled a {roll}')
+print(f'You have rolled a total of {dice_sum}')
+  
+  
+  
+  
+  if __name__== "__main__":
 main()
   
   git add dice_roller.py
